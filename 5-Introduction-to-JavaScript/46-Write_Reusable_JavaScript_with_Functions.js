9036e38984ab85ad590cffac24eb041183ae7c45
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 19:16:54 -0400

    Add code for the forty-sixth of the JavaScript module and comment what I've learned.

---
+++ 5-Introduction-to-JavaScript/46-Write_Reusable_JavaScript_with_Functions.js
@@ -0,0 +1,13 @@
+// I leaned how to write reusable JavaScript with Functions
+// Example
+function ourReusableFunction() {
+  console.log("Heyya, World");
+}
+
+ourReusableFunction();
+
+// Only change code below this line
+function reusableFunction(){
+    console.log("Hi World");
+}
+reusableFunction();
