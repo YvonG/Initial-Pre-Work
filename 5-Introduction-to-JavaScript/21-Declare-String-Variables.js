9e4a6b79637f392a8231cab279e43bc663c24b4a
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 13:58:20 -0400

    Add code for the twenty-first JavaScript module and comment what I've learned.

---
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -0,0 +1,8 @@
+// I learned how to declare string variables
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName="Yvon";
+var myLastName="Nsouadi";
