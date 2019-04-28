396b897fb8d9b8d3859309c0b00e935d9f9cc45a
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 15:30:07 -0400

    Add code for the twenty-seventh JavaScript module and comment what I've learned.

---
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -0,0 +1,8 @@
+// I learned how to use constructing strings with variables.
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName ="Yvon";
+var myStr = "My name is " + myName + " and I am well!";
