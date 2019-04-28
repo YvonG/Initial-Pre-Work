eb137f3b38340e5470a67735c8538c93f9c1a293
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 11:32:17 -0400

    Add code for the fifth JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -0,0 +1,13 @@
+// I learned how to initialize variables
+// Initialize these three variables
+var a;
+var b;
+var c;
+
+// Do not change code below this line
+a=5;
+b=10;
+c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
