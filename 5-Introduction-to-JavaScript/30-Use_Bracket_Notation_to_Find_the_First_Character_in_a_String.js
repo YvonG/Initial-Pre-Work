b7e3f865fff286164db9abf0fedfdd7ded94ac91
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 16:29:34 -0400

    Add code for the thirtieth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -0,0 +1,13 @@
+// I learned how to use bracket notation to find the fist character in a string
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
