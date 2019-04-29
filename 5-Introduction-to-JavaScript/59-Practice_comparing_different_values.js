f0a090655cbbc89742a546ed9dfe88e360e59fee
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 21:14:34 -0400

    Add code for the fifty-ninth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -0,0 +1,11 @@
+// I learned how to compare differentes variables
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
