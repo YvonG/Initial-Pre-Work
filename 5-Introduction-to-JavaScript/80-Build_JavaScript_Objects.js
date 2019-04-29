7a01a01ea038e6b53a0238032d7f689241cce9e4
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-29 10:36:12 -0400

    Add code for the eightieth JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/80-Build_JavaScript_Objects.js
@@ -0,0 +1,17 @@
+// I learned how to to build JavaScript objects
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name":"Minion",
+  "legs":4,
+  "tails":1,
+  "friends":["Affenpinscher","Basset Hound","Cairn Terrier","Dachshund", "Harrier"]
+};
