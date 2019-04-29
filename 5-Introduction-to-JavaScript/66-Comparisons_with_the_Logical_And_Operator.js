cdc25a376e60a0fe850e77cf68a98a8fe553eba5
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 21:52:08 -0400

    Add code for the sixty-sixth JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -0,0 +1,14 @@
+//I learned how to compare with the logical and operator
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val >= 25 && val<=50) {
+    return "Yes";
+    }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
