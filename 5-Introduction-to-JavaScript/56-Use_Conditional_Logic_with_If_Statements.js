f2755a72592753d26294de632640106b6ce6fb61
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 21:01:57 -0400

    Add code for the fifty-sixth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -0,0 +1,24 @@
+// I learned how to used conditional logic with if statements
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+  if (wasThatTrue) {
+    return "Yes, that was true";
+  }
+  // Only change code below this line.
+
+  return "No, that was false";
+
+  // Only change code above this line.
+
+}
+
+// Change this value to test
+trueOrFalse(true);
