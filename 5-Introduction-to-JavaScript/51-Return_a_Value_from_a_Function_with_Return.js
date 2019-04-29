583680a031af42122b58f90b5126531897fe093f
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 20:10:15 -0400

    Add code for the fifty-first of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -0,0 +1,13 @@
+// I learned how to return a value from a function with return
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+function timesFive(nbr){
+  return nbr*5;
+}
+console.log(timesFive(5));
+
+console.log(minusSeven(10));
