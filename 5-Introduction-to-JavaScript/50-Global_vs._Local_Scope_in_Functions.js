56ec23d53359da7bdcdd277f22b2c15c494e41fd
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 20:00:04 -0400

    Add code for the fiftieth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -0,0 +1,12 @@
+// I learned how to use global vs local scope in functions
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear ="sweater";
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
