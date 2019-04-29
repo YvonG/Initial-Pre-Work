cba6a81feabb8a8a93c8e4f6d3de491658f4ba16
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 22:00:33 -0400

    Add code for the sixty-seventh JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -0,0 +1,14 @@
+// I learned how to compare with the logical or operator
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val <10 || val > 20 ) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
