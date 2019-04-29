1af45ec26a684f690b81737d38f7d0e59a271547
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 21:09:36 -0400

    Add code for the fifty-eighth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -0,0 +1,11 @@
+// I learned how to compare with the strict equality operator
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
