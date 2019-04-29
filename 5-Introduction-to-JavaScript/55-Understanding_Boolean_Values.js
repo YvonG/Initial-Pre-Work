a764878ca30b14651855118b35bd0b526b9a1cf2
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 20:36:41 -0400

    Add code for the fifty-fifth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -0,0 +1,9 @@
+// I learned about boolean values
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
