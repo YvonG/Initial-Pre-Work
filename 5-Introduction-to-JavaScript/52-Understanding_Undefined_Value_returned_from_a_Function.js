f613482bec6fa0134483f7b56380e4c6ccf9c3d0
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 20:16:05 -0400

    Add code for the fifty second of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -0,0 +1,15 @@
+// I learned how to undefine value returned from a function
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum = sum + 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
