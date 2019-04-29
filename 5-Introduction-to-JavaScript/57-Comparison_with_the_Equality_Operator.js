6aef3ef082cddbf726bd98d5536613dc57f26c14
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-28 21:06:18 -0400

    Add code for the fifty seventh of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -0,0 +1,11 @@
+// I learned how to compare with the equality operator
+// Setup
+function testEqual(val) {
+  if (val==12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
