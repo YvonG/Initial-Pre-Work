a8a2fbd99cd9e530eedefe55fbe9db96610e16d1
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 14:33:13 -0400

    Add code for the fourteenth of the ES6 module and comment on what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/14-Use-Destructuring-Assignment-to-Assign-Variables-from-Arrays.js
@@ -0,0 +1,10 @@
+// I learned how to use the destructuring assignement to assign variables from arrays
+let a = 8, b = 6;
+(() => {
+  "use strict";
+  // change code below this line
+  [a,b]=[6,8];
+  // change code above this line
+})();
+console.log(a); // should be 6
+console.log(b); // should be 8
