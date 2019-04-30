f945f1c9cd63aacb0f55143b1cf2fee2b8f96b13
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 11:27:56 -0400

    Add code for the first of the ES6 module and comment what I learned

--- 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -1,3 +1,11 @@
-/// I learned how to use different comments.
-/* Multi-line comment*/
-// this is an in-line comment
+// I learned how to explore differences between the var and let keywords
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+   catName = "Oliver";
+   quote = catName + " says Meow!";
+
+}
+catTalk();
