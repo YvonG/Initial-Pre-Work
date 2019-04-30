815f8fad551e5fc1ac3c58fce03fc7374057156f
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 15:33:14 -0400

    Add code for the nineteenth of the ES6 module and comment on what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -0,0 +1,12 @@
+// I learned how to write concise declarative functions with ES6
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
