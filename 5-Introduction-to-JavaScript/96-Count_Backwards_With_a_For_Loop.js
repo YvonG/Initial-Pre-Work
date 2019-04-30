dc1411dfea7e882e3a77768eb5c5a4d552760ab5
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 08:42:09 -0400

    Add code for the ninety-sixth of the JavaScript module and comment my knowledge

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,17 @@
+// I learned how to count backwards with a for lookup
+// Example
+var ourArray = [];
 
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+var myArray = [];
+for (var i=9; i > 0; i-=2) {
+  myArray.push(i);
+}
