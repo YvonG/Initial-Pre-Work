3f9cc7b0b69ab40f0621ec39d39af571ba00de3b
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 13:19:35 -0400

    Add code for the tenth of the ES6 module and comment what I learned

---
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -0,0 +1,8 @@
+// I learned how to use the rest operator with function parameter
+const sum = (function() {
+  "use strict";
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
