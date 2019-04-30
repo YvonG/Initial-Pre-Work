82e6594d9df461530f515372a64a20f926513d97
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-30 00:32:30 -0400

    Add code for the ninety-fourth of the JavaScript module and comment my knowledge

---
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -0,0 +1,15 @@
+// I learned how to iterate with JavaScript For lookups
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 0; i < 5; i++) {
+  myArray.push(i+1);
+}
