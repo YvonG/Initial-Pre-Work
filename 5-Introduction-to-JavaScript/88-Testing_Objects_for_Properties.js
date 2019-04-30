8723e8d014d4ed0e891d779d373f72b6ee722378
YvonG <49823016+YvonG@users.noreply.github.com>
2019-04-29 23:16:50 -0400

    Add code for the eighty-eighth of the JavaScript module and comment what I learned.

---
+++ 5-Introduction-to-JavaScript/88-Testing_Objects_for_Properties.js
@@ -0,0 +1,23 @@
+// I learned how testing objects for properties
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  if (myObj.hasOwnProperty(checkProp) == true) {
+    return myObj[checkProp];
+  }
+  else {
+      return "Not Found"
+  }
+// Test your code by modifying these values
+checkObj("gift");
+  return "Change Me!";
+}
+
+// Test your code by modifying these values
+checkObj("gift");
